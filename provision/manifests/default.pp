8e69493c1aac0c2861a17d49a01f2186e35c7c6d
Matt McManus <mattmcmanus@gmail.com>
2013-04-04 15:06:10 -0400

    Lots of mysql settings tweaks and new TODO file

--- provision/manifests/default.pp
+++ provision/manifests/default.pp
@@ -25,10 +25,7 @@ class { 'mysql::server':
   }
 }
 
-# database { ['kessler', 'kessler_test', 'barankay', 'barankay_test', 'obesity', 'obesity_test']:
-#   ensure   => $ensure,
-# }
-
+# This took a while to figure out. http://stackoverflow.com/a/1559992/109589
 database_user { 'root@localhost':
   password_hash => mysql_password('root')
 }
@@ -39,34 +36,34 @@ database_grant { 'root@%':
   privileges => ['all']
 }
 
-# mysql::server::config { 'performance_tuning':
-#     settings => {
-#       'mysqld' => {
-#         'query_cache_limit'             => '8M',
-#         'query_cache_size'              => '256M',
-#         'slow_query_log '               => '/var/log/mysql/mysql-slow.log',
-#         'long_query_time '              => '10',
-#         'log-queries-not-using-indexes' => true,
-#         'wait_timeout'                  => '60',
-#         'connect_timeout'               => '10',    #Increase connect_timeout from 5 to 10',
-#         'interactive_timeout'           => '120',   #Decrease interactive_timeout from 28800 to 120',
-#         'innodb_buffer_pool_size '      => '128M',
-#         'join_buffer_size'              => '2M',   #Increase join_buffer_size from 131072 to 2M',
-#         'key_buffer_size '              => '64M',
-#         'read_buffer_size'              => '2M',     #Increase read buffer size from 1M',
-#         'read_rnd_buffer_size'          => '4M',     #Increase read_rnd_buffer_size to 4M',
-#         'myisam_sort_buffer_size'       => '64M',
-#         'sort_buffer_size'              => '2M',   #Increase sort buffer size from 1M',
-#         'table_cache'                   => '4096',    #Increase table cache cache from 256 to 2048',
-#         'thread_cache_size'             => '8',
-#         'thread_concurrency'            => '4',  # Try number of CPU's*2 for thread_concurrency',
-#         'tmp_table_size'                => '128M',
-#         'max_heap_table_size'           => '128M',
-#         'max_allowed_packet'            => '16M',    #Increase max allowed packet size from 1M to 16M',
-#         'open_files_limit'              => '3072'
-#       }
-#     }
-#   }
+mysql::server::config { 'performance_tuning':
+    settings => {
+      'mysqld' => {
+        'query_cache_limit'             => '8M',
+        'query_cache_size'              => '256M',
+        'slow_query_log '               => '/var/log/mysql/mysql-slow.log',
+        'long_query_time '              => '10',
+        'log-queries-not-using-indexes' => true,
+        'wait_timeout'                  => '60',
+        'connect_timeout'               => '10',    #Increase connect_timeout from 5 to 10',
+        'interactive_timeout'           => '120',   #Decrease interactive_timeout from 28800 to 120',
+        'innodb_buffer_pool_size '      => '128M',
+        'join_buffer_size'              => '2M',   #Increase join_buffer_size from 131072 to 2M',
+        'key_buffer_size '              => '64M',
+        'read_buffer_size'              => '2M',     #Increase read buffer size from 1M',
+        'read_rnd_buffer_size'          => '4M',     #Increase read_rnd_buffer_size to 4M',
+        'myisam_sort_buffer_size'       => '64M',
+        'sort_buffer_size'              => '2M',   #Increase sort buffer size from 1M',
+        'table_cache'                   => '4096',    #Increase table cache cache from 256 to 2048',
+        'thread_cache_size'             => '8',
+        'thread_concurrency'            => '4',  # Try number of CPU's*2 for thread_concurrency',
+        'tmp_table_size'                => '128M',
+        'max_heap_table_size'           => '128M',
+        'max_allowed_packet'            => '16M',    #Increase max allowed packet size from 1M to 16M',
+        'open_files_limit'              => '3072'
+      }
+    }
+  }
 
 #                 Apache 
 # - - - - - - - - - - - - - - - - - - - - - - -
